5d881c271dac50f4223bf2cbf9c6840902635246
motalaat <motalaat1610@gmail.com>
2024-10-04 12:28:38 +0300

    first commit

---
+++ lib/data/json.dart
@@ -0,0 +1,115 @@
+var doctors = [
+  {
+    "image":
+        "https://images.unsplash.com/photo-1537368910025-700350fe46c7?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1770&q=80",
+    "name": "Dr. Muhammad ta",
+    "skill": "Dermatologists",
+    "review": 540
+  },
+  {
+    "image":
+        "https://images.unsplash.com/photo-1612349317150-e413f6a5b16d?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1770&q=80",
+    "name": "Dr. Mina farid",
+    "skill": "Neurologists",
+    "review": 310
+  },
+  {
+    "image":
+        "https://images.unsplash.com/photo-1625498542602-6bfb30f39b3f?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=774&q=80",
+    "name": "Dr. Terry Henery",
+    "skill": "Dentist",
+    "review": 310
+  },
+  {
+    "image":
+        "https://images.unsplash.com/photo-1618498082410-b4aa22193b38?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1770&q=80",
+    "name": "Dr. Mo Salah",
+    "skill": "Dentist",
+    "review": 310
+  },
+  {
+    "image":
+        "https://images.unsplash.com/photo-1622253692010-333f2da6031d?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=928&q=80",
+    "name": "Dr. Ammar Ali",
+    "skill": "Neurologists",
+    "review": 310
+  },
+  {
+    "image":
+        "https://images.unsplash.com/photo-1559839734-2b71ea197ec2?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1770&q=80",
+    "name": "Dr. Mo AboTrika",
+    "skill": "Neurologists",
+    "review": 310
+  },
+  {
+    "image":
+        "https://images.unsplash.com/photo-1594824476967-48c8b964273f?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=774&q=80",
+    "name": "Dr. Ramirez Galal",
+    "skill": "Neurologists",
+    "review": 310
+  },
+  {
+    "image":
+        "https://images.unsplash.com/photo-1591604021695-0c69b7c05981?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1770&q=80",
+    "name": "Dr. Waleed Ali",
+    "skill": "Neurologists",
+    "review": 310
+  },
+];
+
+var chatsData = [
+  {
+    "image":
+        "https://images.unsplash.com/photo-1537368910025-700350fe46c7?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1770&q=80",
+    "name": "Dr. zakaria Eldardery",
+    "skill": "Dermatologists",
+    "last_text":
+        "Lorem ipsum is a placeholder text commonly used to demonstrate the visual form of a document",
+    "date": "1 min ago",
+  },
+  {
+    "image":
+        "https://images.unsplash.com/photo-1612349317150-e413f6a5b16d?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1770&q=80",
+    "name": "Dr. Ramadan Ali",
+    "skill": "Neurologists",
+    "last_text":
+        "Lorem ipsum is a placeholder text commonly used to demonstrate the visual form of a document",
+    "date": "1 min ago",
+  },
+  {
+    "image":
+        "https://images.unsplash.com/photo-1591604021695-0c69b7c05981?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1770&q=80",
+    "name": "Dr. ahmed nasr",
+    "skill": "Neurologists",
+    "last_text":
+        "Lorem ipsum is a placeholder text commonly used to demonstrate the visual form of a document",
+    "date": "1 min ago",
+  },
+  {
+    "image":
+        "https://images.unsplash.com/photo-1559839734-2b71ea197ec2?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1770&q=80",
+    "name": "Dr. Gamal Nasr",
+    "skill": "Neurologists",
+    "last_text":
+        "Lorem ipsum is a placeholder text commonly used to demonstrate the visual form of a document",
+    "date": "1 min ago",
+  },
+  {
+    "image":
+        "https://images.unsplash.com/photo-1625498542602-6bfb30f39b3f?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=774&q=80",
+    "name": "Dr. Mahmoud Elsaied",
+    "skill": "Dentist",
+    "last_text":
+        "Lorem ipsum is a placeholder text commonly used to demonstrate the visual form of a document",
+    "date": "1 min ago",
+  },
+  {
+    "image":
+        "https://images.unsplash.com/photo-1622253692010-333f2da6031d?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=928&q=80",
+    "name": "Dr. Aref mosh Aref",
+    "skill": "Neurologists",
+    "last_text":
+        "Lorem ipsum is a placeholder text commonly used to demonstrate the visual form of a document",
+    "date": "1 min ago",
+  },
+];
